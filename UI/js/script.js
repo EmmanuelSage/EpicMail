3de19c6b1cd7d9c44b5f15102368969169093e65
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-03-01 01:32:10 +0100

    Set up create group form

--- UI/js/script.js
+++ UI/js/script.js
@@ -3,16 +3,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const dashMenu = document.getElementById("dash-menu-id");
   const closeMobileMenu = document.getElementById("close-mobile-menu");
   const hamburger = document.getElementById("hamburger-link");
+  const createGroupForm = document.getElementById("create-group-id");
 
   if (hamburger) {
     hamburger.addEventListener("click", () => {
-      dashMenu.style.display = "block";
+      let displayState = dashMenu.style.display;
+      if (displayState === "none") {
+        dashMenu.style.display = "block";
+        if (createGroupForm) {
+          createGroupForm.style.display = "none";
+        }
+      }
+      else {
+        dashMenu.style.display = "none";
+        if (createGroupForm) {
+          createGroupForm.style.display = "block";
+        }
+      }
+
     });
   }
 
   if (closeMobileMenu) {
     closeMobileMenu.addEventListener("click", () => {
       dashMenu.style.display = "none";
+      if (createGroupForm) {
+        createGroupForm.style.display = "block";
+      }
     });
   }
 
