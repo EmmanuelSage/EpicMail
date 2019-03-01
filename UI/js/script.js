0476eb3349b384bee9a2616ae1db13b1fd3264ca
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-03-01 13:34:09 +0100

    Add javascript to show thread messages

--- UI/js/script.js
+++ UI/js/script.js
@@ -49,6 +49,21 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 
+  const thread = document.getElementsByClassName("thread");
 
+  for (let i = 0; i < thread.length; i++) {
+    thread[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+
+      var panel = this.nextElementSibling;
+
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+
+    });
+  }
 
 });
