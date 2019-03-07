d567e2fd3c0b043732497d9115349e8015dee6b9
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-03-07 14:00:01 +0100

    feat(user): Write on success test
    - Define User can send message to individuals suite
    - write should Post Messages if details are correct test
    - write should Assign 0 to parentMessageId if not given test
    
    [Delivers 164427617]

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import user from '../controllers/user';
 import userValidator from '../middlewares/validateUser';
+import Auth from '../middlewares/Auth';
+import messages from '../controllers/messages';
 
 const router = express.Router();
 
 router.post('/auth/signup', userValidator.verifyUser, user.create);
 router.post('/auth/login', userValidator.verifyLogin, user.login);
+router.post('/messages', Auth.verifyToken, messages.create);
 
 export default router;
---
+++ server/test/messages.test.js
@@ -0,0 +1,84 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+let authToken;
+
+describe('User can send message to individuals ', () => {
+  before((done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        firstName: 'david',
+        lastName: 'Oluyale',
+        email: 'david@gmail.com',
+        password: 'david1',
+      })
+      .end((err, res) => {
+        authToken = res.body.data[0].token;
+        done(err);
+      });
+  });
+
+  it('Should Post Messages if details are correct', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/messages')
+      .set('x-access-token', authToken)
+      .send({
+        subject: 'The Weather',
+        message: 'Lagos is very hot this days, like what',
+        receiverId: '2',
+        parentMessageId: '1',
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body.status).to.be.equal(201);
+        expect(res.body.data).to.deep.equal([
+          {
+            id: res.body.data[0].id,
+            createdOn: res.body.data[0].createdOn,
+            subject: 'The Weather',
+            message: 'Lagos is very hot this days, like what',
+            receiverId: 2,
+            parentMessageId: 1,
+          },
+        ]);
+        done(err);
+      });
+  });
+
+  it('Should Assign 0 to parentMessageId if not given', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/messages')
+      .set('x-access-token', authToken)
+      .send({
+        subject: 'The Weather',
+        message: 'Lagos is very hot this days, like what',
+        receiverId: '2',
+        parentMessageId: '',
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body.status).to.be.equal(201);
+        expect(res.body.data[0].parentMessageId).to.be.equal(0);
+        expect(res.body.data).to.deep.equal([
+          {
+            id: res.body.data[0].id,
+            createdOn: res.body.data[0].createdOn,
+            subject: 'The Weather',
+            message: 'Lagos is very hot this days, like what',
+            receiverId: 2,
+            parentMessageId: 0,
+          },
+        ]);
+        done(err);
+      });
+  });
+});
