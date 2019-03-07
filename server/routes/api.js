6e73486857a60f2a230dfef62895fdc39cdad476
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-03-07 14:42:49 +0100

    feat(user): implement validateMessages middleware
    - Write condition to ensure user enters a subject
    - Write condition to ensure user enters a message
    - Write condition to ensure user enters a receiverId
    
    [Delivers 164427617]

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,12 @@ import user from '../controllers/user';
 import userValidator from '../middlewares/validateUser';
 import Auth from '../middlewares/Auth';
 import messages from '../controllers/messages';
+import validateMessages from '../middlewares/validateMessages';
 
 const router = express.Router();
 
 router.post('/auth/signup', userValidator.verifyUser, user.create);
 router.post('/auth/login', userValidator.verifyLogin, user.login);
-router.post('/auth/messages', Auth.verifyToken, messages.create);
+router.post('/auth/messages', Auth.verifyToken, validateMessages.verifyMessage, messages.create);
 
 export default router;
