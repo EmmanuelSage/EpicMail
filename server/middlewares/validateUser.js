f3dae5a62094a5f5a7595f00cf3ee7a16357b21a
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-03-07 06:15:43 +0100

    feat(user): Add validateUser middleware
    - Add validate user middleware for helper class
    
    [Delivers #164427202]

---
+++ server/middlewares/validateUser.js
@@ -0,0 +1,23 @@
+import Helper from '../utilities/helper';
+
+const userValidator = {
+  verifyUser(req, res, next) {
+    if (!Helper.isValidEmail(req.body.email)) {
+      return res.status(400).send({ status: 400, error: 'Please enter a valid email address' });
+    }
+    if (!Helper.isValidName(req.body.firstName)) {
+      return res.status(400).send({ status: 400, error: 'Please enter a valid firstName' });
+    }
+    if (!Helper.isValidName(req.body.lastName)) {
+      return res.status(400).send({ status: 400, error: 'Please enter a valid lastName' });
+    }
+    if (req.body.password.length < 6) {
+      return res.status(400).send({ status: 400, error: 'Please enter a password of at least six characters' });
+    }
+
+    return next();
+  },
+};
+
+
+export default userValidator;
