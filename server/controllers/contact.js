fdf15446d02a7fef845d10ce4b14b654cda0e31d
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-03-08 21:54:16 +0100

    feat(user): Write Code to pass test for user create contact
     - Implement methods to controllers and models contact.js to pass test
     - run tests to ensure they are passing
    
     [Delivers #164507408]

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -2,7 +2,22 @@ import db from '../models/Contact';
 
 const Contact = {
 
-  create() {
+  create(req, res) {
+    const reqContact = {
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    };
+
+    const newContact = db.create(reqContact);
+
+    return res.status(201).send({
+      status: 201,
+      data: [{
+        newContact,
+        message: `Contact ${newContact.firstName} ${newContact.lastName} has been created.`,
+      }],
+    });
   },
 
 };
--- server/models/Contact.js
+++ server/models/Contact.js
@@ -3,8 +3,18 @@ class Contact {
     this.contacts = [];
   }
 
-  create() {
-    this.notImplemented = '';
+  create(data) {
+    const numOfContacts = (this.contacts.length);
+    const countId = (numOfContacts === 0) ? 1 : (this.contacts[numOfContacts - 1].id + 1);
+    const newContact = {
+      id: countId,
+      email: data.email,
+      firstName: data.firstName,
+      lastName: data.lastName,
+    };
+
+    this.contacts.push(newContact);
+    return newContact;
   }
 }
 
