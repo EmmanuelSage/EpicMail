27070e2dcd2fc890647ebc94d50fa6aee1bd0daf
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-03-10 16:29:12 +0100

    added api-docs route

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ import apiRoutes from './routes/api';
 const app = express();
 
 app.use(cors());
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
 const PORT = process.env.PORT || 7000;
@@ -21,12 +21,12 @@ app.use('/api/v1', apiRoutes);
 //   next();
 // });
 
-// app.get('/', (req, res) => {
-//   res.status(200).send({
-//     status: 200,
-//     data: { message: 'Welcome to Epic Mail. visit /api/v1 for api.' },
-//   });
-// });
+app.get('/', (req, res) => {
+  res.status(200).send({
+    status: 200,
+    data: { message: 'Welcome to Epic Mail. visit /api-docs for api documentation.' },
+  });
+});
 
 app.get('*', (req, res) => {
   res.status(404).send({
