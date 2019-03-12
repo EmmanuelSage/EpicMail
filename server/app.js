f6b1e8a66c5b937966722c26f2a5913a2c96aac7
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-03-12 07:31:35 +0100

    feat(user): Implement api documentation
    - Install swagger-ui-express
    - Write swagger.yml on editor and export to json
    - Install cors to allow cross origin resource sharing
    - update email not found from 400 to 404 error
    
    [Delivers #164508294]

--- server/app.js
+++ server/app.js
@@ -1,17 +1,25 @@
 import express from 'express';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 import apiRoutes from './routes/api';
 
+
 const app = express();
+
+app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
 const PORT = process.env.PORT || 7000;
 
 app.use('/api/v1', apiRoutes);
 
+
 app.get('/', (req, res) => {
   res.status(200).send({
     status: 200,
-    data: { message: 'Welcome to Epic Mail. visit /api/v1 for api.' },
+    data: { message: 'Welcome to Epic Mail. visit /api-docs for api. documentation' },
   });
 });
 
